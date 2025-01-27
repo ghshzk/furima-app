9ffcc8744ec38091c0f7effcccb486268bb4a60c
honoka <mintbeer0924@gmail.com>
2025-01-27 20:30:49 +0900

    マイグレーションファイルの作成１

--- src/database/migrations/2025_01_22_201104_create_orders_table.php
+++ src/database/migrations/2025_01_22_201104_create_orders_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateOrdersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('item_id')->constrained()->cascadeOnDelete();
+            $table->decimal('price',10,2);
+            $table->tinyInteger('payment_method');
             $table->string('address');
-            $table->string('image_url')->nullable();
-            $table->rememberToken();
+            $table->tinyInteger('status');
             $table->timestamps();
         });
     }
@@ -33,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('orders');
     }
 }
