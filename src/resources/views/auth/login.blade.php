c8d506b0c4bcbdb19704c04b18c3f7c98beafa9b
honoka <mintbeer0924@gmail.com>
2025-02-05 12:25:29 +0900

    ログイン画面のViewの修正

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -14,19 +14,25 @@
                 <label class="login-form__label" for="email">ユーザー名/メールアドレス</label>
                 <input class="login-form__input" type="email" id="email" name="email">
                 <p class="login-form__error-message">
-                    エラー
+                    @error('email')
+                    {{ $message }}
+                    @enderror
                 </p>
             </div>
             <div class="login-form__group">
                 <label class="login-form__label" for="password">パスワード</label>
                 <input class="login-form__input" type="password" id="password" name="password">
                 <p class="login-form__error-message">
-                    エラー
+                    @error('password')
+                    {{ $message }}
+                    @enderror
                 </p>
             </div>
             <button class="login-form__btn btn" type="submit">ログインする</button>
         </form>
-        <a class="login-form__link link" href="/register">会員登録はこちら</a>
+        <div class="login-form__link-container">
+            <a class="login-form__link link" href="/register">会員登録はこちら</a>
+        </div>
     </div>
 </div>
 @endsection
