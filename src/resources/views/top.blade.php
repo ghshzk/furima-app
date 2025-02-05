a5226ffadee792d6274935b323521fb427ee62ee
honoka <mintbeer0924@gmail.com>
2025-02-05 19:59:54 +0900

    トップ画面のヘッダー修正

--- src/resources/views/top.blade.php
+++ src/resources/views/top.blade.php
@@ -5,7 +5,27 @@
 @endsection
 
 @section('header-nav')
+<div class="header-search-form">
+    <form class="header-search-form__form" action="" method="get">
+        @csrf
+        <input class="header-search-form__input" type="text" value="{{ request('search') }}" placeholder="なにをお探しですか？">
+        <button class="header-search-form__button" type="submit">検索</button>
+    </form>
+</div>
 
+<nav class="header-nav">
+    <ul class="header-nav__list">
+        <li class="header-nav__item">
+            <a class="header-nav__link" href="/login">ログイン</a>
+        </li>
+        <li class="header-nav__item">
+            <a class="header-nav__link" href="/mypage">マイページ</a>
+        </li>
+        <li class="header-nav__item">
+            <a class="header-nav__link header-nav__link-sell" href="/sell">出品</a>
+        </li>
+    </ul>
+</nav>
 @endsection
 
 @section('content')
