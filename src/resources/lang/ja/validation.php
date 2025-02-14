2b3fc902869adadd3f648f0c15be019ed1a5be9a
honoka <mintbeer0924@gmail.com>
2025-02-14 22:35:37 +0900

    リクエスト修正

--- src/resources/lang/ja/validation.php
+++ src/resources/lang/ja/validation.php
@@ -38,7 +38,7 @@ return [
     'digits_between'       => ':attributeは、:min桁から:max桁にしてください。',
     'dimensions'           => ':attributeの画像サイズが無効です',
     'distinct'             => ':attributeの値が重複しています。',
-    'email'                => ':attributeは、有効なメールアドレス形式で指定してください。',
+    'email'                => ':attributeはメール形式で入力してください',
     'ends_with'            => ':attributeは、次のうちのいずれかで終わらなければなりません。: :values',
     'exists'               => '選択された:attributeは、有効ではありません。',
     'file'                 => ':attributeはファイルでなければいけません。',
@@ -96,7 +96,7 @@ return [
     'password'             => 'パスワードが正しくありません。',
     'present'              => ':attributeが存在している必要があります。',
     'regex'                => ':attributeには、有効な正規表現を指定してください。',
-    'required'             => ':attributeは、必ず指定してください。',
+    'required'             => ':attributeを入力してください',
     'required_if'          => ':otherが:valueの場合、:attributeを指定してください。',
     'required_unless'      => ':otherが:values以外の場合、:attributeを指定してください。',
     'required_with'        => ':valuesが指定されている場合、:attributeも指定してください。',
@@ -133,12 +133,7 @@ return [
         'attribute-name' => [
             'rule-name' => 'custom-message',
         ],
-        'email' => [
-            'required' => 'メールアドレスを入力してください',
-            'email' => 'メールアドレスは「ユーザー名@ドメイン」形式で入力してください',
-        ],
         'password' => [
-            'required' => 'パスワードを入力してください',
             'min' => 'パスワードは8文字以上で入力してください'
         ],
     ],
